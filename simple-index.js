04da301c60c474f3476ab9c37e51b0da128e6a74
Jeremy Sellars <jeremy.sellars@softekinc.com>
2012-07-08 08:12:01 -0500

    Exposed types

--- simple-index.js
+++ simple-index.js
@@ -1,4 +1,11 @@
 module.exports = {
-  Index: require("./Index.js").Index,
-  DocumentInverter: require("./DocumentInverter.coffee").DocumentInverter
+  Index: require("./lib/Index.js").Index
+  ,DedupFilter: require("./lib/DedupFilter.js").DedupFilter
+  ,DocumentBuilder: require("./lib/DocumentBuilder.js").DocumentBuilder
+  ,DocumentInverter: require("./lib/DocumentInverter.js").DocumentInverter
+  ,IndexSearcher: require("./lib/IndexSearcher.js").IndexSearcher
+  ,LowerCaseFilter: require("./lib/LowerCaseFilter.js").LowerCaseFilter
+  ,PrefixFilter: require("./lib/PrefixFilter.js").PrefixFilter
+  ,Query: require("./lib/Query.js").Query
+  ,StopWordFilter: require("./lib/StopWordFilter.js").StopWordFilter
 }
